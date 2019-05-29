6e8fb2e10c4820fc2c30241fd06fd261b61335bc
psmagicman <julienlaw1@gmail.com>
2019-05-29 14:42:47 +0900

    add demo frontend files to webserver

---
+++ wordguess/static/precache-manifest.1ba8a1e6e38f110892a4e87961786bce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e54951a6a8f870b7d57f3cf65352f57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8ece070d51f905657a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b0e2adf4fe5b8c856d30",
+    "url": "/static/js/2.809a336d.chunk.js"
+  },
+  {
+    "revision": "fe8ece070d51f905657a",
+    "url": "/static/js/main.ec070cba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
